7c127271d5f7703acc35ec6b354f0f8f9d7f9862
valost <valerieostrovska@yahoo.com>
2024-07-13 21:41:09 +0300

    make build

---
+++ index.4d0c4085.js
@@ -0,0 +1,2 @@
+document.querySelectorAll("tr").forEach(function(e){var l=e.querySelectorAll("th, td"),o=l[1].cloneNode(!0),r=l.length-1;l[r].before(o)});
+//# sourceMappingURL=index.4d0c4085.js.map
